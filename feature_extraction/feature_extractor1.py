fixed wrong normalization

feature_extraction/feature_extractor1.py
@@ -46,7 +46,7 @@ def extract_features_type1(X, num_frames):
 	features[:, 12, :] = tools.diff_position_x(X[:, :, :, 0], num_frames)
 	features[:, 13, :] = tools.diff_position_y(X[:, :, :, 0], num_frames)
 	# normalize and round
-	features = tools.normalize(features, num_frames)
+	# features = tools.normalize(features, num_frames)
 	features = np.around(features, decimals=3)
 
 	return features

feature_extraction/tools.py
@@ -9,8 +9,20 @@ def normalize(X, valid_frame_num):
     out = np.zeros((N, feat, T))
     for i in range(N):
         for f in range(feat):
-            max_feat = np.max(np.absolute(X[i, f, :]))
-            out[i, f, :] = X[i, f, :] * (1/max_feat)
+            denom = np.max(X[i, f, :]) - np.min(X[i, f, :])
+            out[i, f, :] = X[i, f, :] * (1/denom)
+    return out
+
+def normalize_allsamples(X):
+    '''
+    normalize for each feature across all samples
+    in_shape = (N, 14)
+    '''
+    N, feat = X.shape
+    out = np.zeros((N, feat))
+    for i in range(feat):
+        denom = np.max(X[:, i]) - np.min(X[:, i])
+        out[:, i] = X[:, i] * (1/denom)
     return out
 
 def normalize_by_height(feature, X, valid_frame_num):

main.py
@@ -77,6 +77,13 @@ def custom_cv_subj(data):
         train_idx = [x for x in range(subjects * actions) if x not in val_idx]
         yield train_idx, val_idx
 
+def total_mean_acc(env_classified, total_acc):
+    total_classified = sum(env_classified)
+    acc = 0
+    for i in range(len(total_acc)):
+        acc += total_acc[i] * env_classified[i] / total_classified
+    return np.around(acc, decimals=2)
+
 def save_model(clf, environment):
     os.makedirs('./models', exist_ok=True)
     with open('./models/{}_final_model.pkl'.format(environment), 'wb') as f:
@@ -97,6 +104,9 @@ if __name__ == '__main__':
     np.random.seed(0)
     random.seed(0)
 
+    env_classified = []
+    total_acc = []
+
     for env in _ENVIRONMENT:
 
         print('')
@@ -111,6 +121,7 @@ if __name__ == '__main__':
         num_frames = dataset.valid_frame_num
 
         X = frame_by_frame_samples(extract_features_type1(X, num_frames), num_frames)
+        X = normalize_allsamples(X)
         Y = frame_by_frame_labels(Y, num_frames)
 
         # print classes distribution
@@ -120,6 +131,7 @@ if __name__ == '__main__':
         for i in range(len(bins)):
             oneh_vector[i] = bins[i] 
         print(oneh_vector)
+        env_classified.append(len(Y))
  
 
         if params.evaluation == 'cv' or 'full':
@@ -131,6 +143,7 @@ if __name__ == '__main__':
             for i in range(4):
                 print(scores[i])
             print("Mean Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+            total_acc.append(scores.mean())
         if params.evaluation == 'full':
             # produces confusion matrices
             clf = svm.SVC(decision_function_shape='ovo', gamma='scale')
@@ -152,5 +165,7 @@ if __name__ == '__main__':
             save_model(clf, env)
             np.set_printoptions(precision=2)
             plot_confusion_matrix(correct_labels, pred_labels, classes=np.array(_CLASS_NAMES), normalize=True, title=env)
-
+    print('')
+    print('Total average accuracy across all environments: ')
+    print(total_mean_acc(env_classified, total_acc))
 